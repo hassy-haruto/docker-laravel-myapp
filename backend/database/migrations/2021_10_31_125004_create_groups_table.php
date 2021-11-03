d16319b8ce0a64954a2cc6a9f278c014359211c2
hassy-haruto <tennsaihagekamisamanoiutouri@gmail.com>
2021-11-03 22:09:14 +0900

    feat update migration

--- backend/database/migrations/2021_10_31_125004_create_groups_table.php
+++ backend/database/migrations/2021_10_31_125004_create_groups_table.php
@@ -14,19 +14,14 @@ class CreateGroupsTable extends Migration
     public function up()
     {
         Schema::create('groups', function (Blueprint $table) {
-            $table->increments('id')->comment('ID');
-            $table->unsignedBigInteger('user_id');
-            $table->string('name',20);
+            $table->increments('id');
+            $table->unsignedBigInteger('group_id');
+            $table->string('name', 25);
             $table->string('picture')->nullable();
             $table->integer('amount')->nullable();
-            $table->integer('activity_frequency')->nullable();
-            $table->string('keyword')->nullable();
             $table->text('content',560)->nullable(); 
+            $table->string('password', 20);
             $table->timestamps();
-            $table->foreign('user_id') //外部キーの宣言
-            ->references('id') //参照先
-            ->on('users') //参照テーブル
-            ->onDelete('cascade'); //参照テーブルカラムが消えたら同時に消す
         });
     }
 
